84dd09a1b3867fafc6c75abdc2da8b2865ca9c29
KGokulKrishnan <gokulkrish3006@gmail.com>
2023-03-09 15:02:50 +0530

    difference

---
+++ Difference.java
@@ -0,0 +1,9 @@
+import javafx.scene.effect.Light.Spot;
+
+public class Difference {
+    public static void main(String[] args) {
+        int x = 12;
+        int y = 10;
+        System.out.println(x - y);
+    }
+}
