d9daab1437f4c499daac91b3dec7c64ba1f478bf
KGokulKrishnan <116270666+KGokulKrishnan@users.noreply.github.com>
2023-03-09 14:57:57 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -2,6 +2,8 @@ public class Add {
     public static void main(String[] args) {
         int x = 10;
         int y = 12;
-        System.out.println(x + y);
+        int a = 20;
+        int b = 10;
+        System.out.println(x + y + a + b);
     }
 }
